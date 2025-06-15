c4669e3f048574d67d57158d438a1065308a2646
Malik Ibo <malik97un@gmail.com>
2025-06-15 18:38:47 +0200

    test java main & calcul

---
+++ src/com/malik/naissance/Calculatrice.java
@@ -0,0 +1,10 @@
+package com.malik.naissance;
+
+public class Calculatrice {
+  
+  public int addition(int entierOne, int entierTwo) {
+    int resultat = entierOne + entierTwo;
+    return resultat;
+  }
+
+}
---
+++ src/com/malik/naissance/Main.java
@@ -0,0 +1,11 @@
+package com.malik.naissance;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculatrice calcul = new Calculatrice();
+        int Total = calcul.addition(4, 8);
+        System.out.println("Hello, depuis le package com.malik.naissance !");
+        System.out.println(Total);
+
+    }
+}
